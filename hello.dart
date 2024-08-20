655186f82bb7620ba17dab6df1827d0bd6d85d55
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-08-20 17:24:05 -0600

    basic1

--- hello.dart
+++ hello.dart
@@ -1,3 +1,18 @@
 void main(){
     print("Helloo World");
+
+String firstName = "fun";
+print("String: $firstName");
+
+int myNum =69;
+print("$myNum");
+
+double otherNum=69.69;
+print("$otherNum");
+
+bool myBool = false;
+print("$myBool");
+
+dynamic fullName = "funStuff";
+print("$fullName");
 }
