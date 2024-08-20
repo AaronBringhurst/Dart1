bbc8a413073b57ffb60e80dbb05828a7e0bd6fd0
Aaron Bringhurst <bringhurst.aaron@gmail.com>
2024-08-20 17:53:46 -0600

    maps

---
+++ maps.dart
@@ -0,0 +1,14 @@
+void main(){
+  var toppings = {"jon":"pepperoni", "Mary":"Cheese"};
+  print(toppings);
+  print(toppings["jon"]);
+
+  print(toppings.values);
+
+  print(toppings.keys);
+
+  print(toppings.length);
+
+  toppings["Tim"] = "Sausage";
+  print(toppings);
+}
